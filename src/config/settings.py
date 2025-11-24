13b893375affcd353c2cce28d563b86b581c3f62
ersaasyhar <ersa.asyhar@gmail.com>
2025-11-24 23:00:35 +0700

    fix: env not match

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 
-
 load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data"
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
